d0d6988d371b489f3793e8d62d5749bafe842151
anchen12197 <anchen12@s.sfusd.edu>
2025-12-18 09:20:22 -0800

    Finished M9L12

--- M09.top_down_movement_workbook/lessons/bouncer.gd
+++ M09.top_down_movement_workbook/lessons/bouncer.gd
@@ -1,26 +1,23 @@
 extends CharacterBody2D
 
-## The top speed that the runner can achieve
 @export var max_speed := 600.0
-## How much speed is added per second when the player presses a movement key
+
 @export var acceleration := 1200.0
-@export var deceleration := 1080.0
 
 @onready var _dust: GPUParticles2D = %Dust
 @onready var _runner_visual: RunnerVisual = %RunnerVisualPurple
 
 func _physics_process(delta: float) -> void:
-	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var has_input_direction := direction.length() > 0.0
-	if has_input_direction:
-		var desired_velocity := direction * max_speed
-		velocity = velocity.move_toward(desired_velocity, acceleration * delta)
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
-
+	var direction := global_position.direction_to(get_global_mouse_position())
+	var distance := global_position.distance_to(get_global_mouse_position())
+	var speed := max_speed if distance > 100 else max_speed * distance / 100
+	
+	var desired_velocity := direction * speed
+	
+	velocity = velocity.move_toward(desired_velocity, acceleration * delta)
 	move_and_slide()
 
-	if direction.length() > 0.0:
+	if direction.length() > 10.0:
 		_runner_visual.angle = rotate_toward(_runner_visual.angle, direction.orthogonal().angle(), 8.0 * delta)
 
 		var current_speed_percent := velocity.length() / max_speed
